f0582d4fb65236dca51ed8fc5b8f70d15b84beff
ConnorHolland <hollandcc@student.swosu.edu>
2015-10-21 11:36:50 -0500

    developed

--- src/development/Sandbox.java
+++ src/development/Sandbox.java
@@ -4,5 +4,6 @@ public class Sandbox {
 	public static void main(String[] args) {
 System.out.println("Hello");
 System.out.println("Git is so easy lol");
+System.out.println("Computer Science is so fun");
 }
 }
